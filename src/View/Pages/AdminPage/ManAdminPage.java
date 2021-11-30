36c684deed0b794e268daf5f2b0759e1de9eefcb
JayGurjar <55961505+JayGurjar@users.noreply.github.com>
2021-11-30 00:54:16 -0700

    Manager View Skeleton

---
+++ src/View/Pages/AdminPage/ManAdminPage.java
@@ -0,0 +1,19 @@
+package View.Pages.AdminPage;
+
+public class ManAdminPage implements AdminPage {
+
+    @Override
+    public void changeFees() {
+        //Change fees logic
+    }
+
+    @Override
+    public void viewDatabase() {
+        //View Database logic
+    }
+
+    @Override
+    public void getReport() {
+        //Report Logic
+    }
+}
