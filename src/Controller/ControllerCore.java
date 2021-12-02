2b811ad0854a705e766cb2dc3213bd6299c09de4
alexishamrak <alhamrak@shaw.ca>
2021-12-02 09:43:38 -0700

    Added basic sql database connection

--- src/Controller/ControllerCore.java
+++ src/Controller/ControllerCore.java
@@ -1,21 +1,35 @@
 package Controller;
 
 import java.util.*;
+import View.ViewCore;
 
 import Model.Property.Address;
 import Model.Property.Property;
 import Model.Role.Landlord;
 import Model.Role.Manager;
 import Model.Role.Person;
+import Controller.DBCore;
 
 public class ControllerCore {
-
+    private ViewCore view;
     Person person;
     ArrayList<Property> properties;
+    DBCore database;
 
     public ControllerCore(){
         System.out.println("Controller Made!");
         this.properties = new ArrayList<Property>();
+    }
+
+    /**
+     * user sends in an email and password. The function checks if this exists in the database and returns a boolean
+     * whether successful or unsuccessful
+     * @param email email to login with
+     * @param password password to login with
+     * @return status of the login
+     */
+    public boolean login(String email, String password){
+
     }
 
     public void setLandlord(){
--- src/Controller/DBCore.java
+++ src/Controller/DBCore.java
@@ -1,5 +1,35 @@
 package Controller;
 
+import java.sql.*;
+
 public class DBCore {
+    private final String DBURL;
+    private final String USERNAME;
+    private final String PASSWORD;
+    public Connection dbConnect;
+    private ResultSet result;
+
+    /**
+     * Constructor for DBCore. Stores the username, password, and database URL
+     * @param url String argument for database url
+     * @param username String argument for username
+     * @param password String argument for password
+     */
+    public DBCore(String url, String username, String password){
+        this.DBURL = url;
+        this.USERNAME = username;
+        this.PASSWORD = password;
+    }
+
+    /**
+     * creates connection to the database. Will catch an exception if the connection is not successful.
+     */
+    public void createConnection(){
+        try{
+            dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
