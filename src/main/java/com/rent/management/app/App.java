e23d3f02fd9ff9ec232f6de240d1c14dfb3d8283
Risat Haque <risathq@gmail.com>
2021-12-02 11:31:13 -0700

    feat: mvn postgres connection

--- src/main/java/com/rent/management/app/App.java
+++ src/main/java/com/rent/management/app/App.java
@@ -1,5 +1,7 @@
 package com.rent.management.app;
 
+import java.sql.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,28 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App myApp = new App();
+        myApp.createConnection();
+    }
+
+    public App(){
+
+    }
+
+    public void createConnection(){
+        Connection dbConnect = null;
+        String DB_HOST = System.getenv("DB_HOST");
+        String DB_PORT = System.getenv("DB_PORT");
+        String DB_NAME = System.getenv("DB_NAME");
+        String DB_USER = System.getenv("DB_USER");
+        String DB_PASS = System.getenv("DB_PASS");
+
+        String DBURL = "jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+        
+        try{
+            dbConnect = DriverManager.getConnection(DBURL, DB_USER, DB_PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
